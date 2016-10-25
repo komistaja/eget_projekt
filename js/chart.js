bc6e416313107f97511004fe229987977bb7d9cf
komistaja <kolistelija@gmail.com>
2016-10-25 10:20:35 +0300

    work on outputchart
    got linewidth to work

--- js/chart.js
+++ js/chart.js
@@ -1,6 +1,5 @@
 // chart 1 td background color
 var td1 = document.getElementsByClassName("td1");
-console.log(td1);
 var i = 0;
 while(i < td1.length) {
     if(td1[i].innerHTML <= 1 && td1[i].innerHTML > 0) { td1[i].style.backgroundColor = "white"; }
@@ -21,13 +20,14 @@ function ingName(input) { // returns ingredient realname from databasevalue
 
 // function for onclick chart 2
 var jsArrayReverse = js_array.reverse();
+ainesclick('feel');
 function ainesclick(ing) {
     document.getElementById("chartcontainer").innerHTML = ''; // empty container
     document.getElementById("chartcontainer").style.visibility = "visible";
     
     var div = document.createElement("div");
-    
-    div.innerHTML = '<h3 class="panel-title">' + ingName(ing) + 'ajalta ' + js_array[13]['date'] + ' - ' + js_array[0]['date'] + '</h3>';
+    var arrLen = jsArrayReverse.length - 1;
+    div.innerHTML = '<h3 class="panel-title">' + ingName(ing) + 'ajalta ' + js_array[arrLen]['date'] + ' - ' + js_array[0]['date'] + '</h3>';
     div.setAttribute("class", "panel-default");
     div.setAttribute("class", "panel-heading");
     
@@ -35,27 +35,26 @@ function ainesclick(ing) {
     
     // creates div rows for table
     var i = 0;
-    while(i < js_array.length && i <= 13) {  // js_array specified in output.php
+    while(i < js_array.length && i <= 30) {  // js_array specified in output.php
         var div = document.createElement("div");
-        div.innerHTML = '<div class="linedate">' + js_array[i]['date'] + '</div>' + js_array[i][ing];
+        div.style.width = js_array[i][ing] * 100 + "px";
+        div.innerHTML = '<div class="linedate">' + js_array[i]['date'] + '</div>' + '<div class="num">' + js_array[i][ing] + '</div>';
         div.setAttribute("onmouseover", "lineMouseOver(this)");
         div.setAttribute("onmouseout", "lineMouseOut(this)");
         div.setAttribute("class", "line");
-        div.style.width = js_array[i][ing] * 100;
+        
         document.getElementById("chartcontainer").appendChild(div);
         i++;
     }
+    
+
     
     
     // chart lines node
     var chartdiv = document.getElementById("chartcontainer").getElementsByClassName("line"); 
     var i = 0;
     
-   /* while(i < chartdiv.length) {
-        chartdiv[i].style.width = chartdiv[i].innerHTML * 100 + "px";
-        
-        i++;
-    } */
+  
     
     // changes text color to black if innerHTML value 0
     var i = 0;
@@ -63,46 +62,23 @@ function ainesclick(ing) {
         if(chartdiv[i].innerHTML == "0") {
             chartdiv[i].style.color = "white";
         }
-        chartdiv[i].style.backgroundColor = "#305899";
+       // chartdiv[i].style.backgroundColor = "#305899";
         i++;
     }
-        
 }
 
 //functions for mouseover chartline
 function lineMouseOver(line) {
-    line.style.width = line.innerHTML * 120 + "px";
+    var lineDiv = line.getElementsByTagName("div");
+    line.style.width = lineDiv[1].innerHTML * 120 + "px";
     line.setAttribute("class", "linemouseon");
 }
 
 function lineMouseOut(line) {
-    line.style.width = line.innerHTML * 100 + "px";
+    var lineDiv = line.getElementsByTagName("div");
+    line.style.width = lineDiv[1].innerHTML * 100 + "px";
     line.setAttribute("class", "line");
-//    line.style.height = "20px";
-//    line.style.backgroundColor = "#305899";
-
-//    line.style.padding = "0px";
-//    line.style.fontSize = "8px";
 }
 
-// create divs for chart values
-/*
-var i = 0;
-var chartdata = document.getElementsByClassName("td1");
-while(i < chartdata.length) {
-    var div = document.createElement("div");
-    div.innerHTML = td1[i].innerHTML;
-    document.getElementById("chartcontainer").appendChild(div);
-    i++;
-}
 
-// set width of chartlines
-var x = document.getElementById("chartcontainer").getElementsByTagName("div");
-var i = 0;
-while (i < x.length) {
-    x[i].style.width = x[i].innerHTML * 100 + "px";
-    x[i].style.backgroundColor = "blue";
-    i++;
-}
-*/
 console.log(js_array);
